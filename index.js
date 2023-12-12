428d98cb1d4de8afef0d5bce50177e726b6c2d6d
German Burgardt <german@getgloby.com>
2023-12-11 22:54:11 -0300

    retry request whisper

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import fs from "fs";
 import readline from "readline";
 import { transcribeAudio } from "./transcriber.js";
 import micConfig from "./micConfig.js";
+import chalk from "chalk";
+import ora from "ora";
+import * as emoji from "node-emoji";
 
 var micInstance = mic(micConfig);
 var outputFileStream = fs.createWriteStream("output.wav");
@@ -10,12 +13,27 @@ var micInputStream = micInstance.getAudioStream();
 var isRecordingFinished = false;
 
 micInputStream.on("data", function (data) {
-  console.log(`Grabando... [${data.length} bytes recibidos]`);
+  console.log(
+    chalk.blue(
+      `Grabando... ${emoji.get("microphone")} [${data.length} bytes recibidos]`
+    )
+  );
 });
 
 micInputStream.pipe(outputFileStream);
 
-console.log("Iniciando grabación... Habla y presiona ENTER para finalizar.");
+console.log(
+  chalk.green("Iniciando grabación... " + emoji.get("arrow_forward"))
+);
+
+const recordingSpinner = ora({
+  text: "Grabando... Presiona ENTER para detener.",
+  color: "yellow",
+  spinner: "dots",
+});
+
+micInstance.start();
+recordingSpinner.start();
 
 micInstance.start();
 
@@ -28,8 +46,8 @@ const rl = readline.createInterface({
 
 rl.on("line", () => {
   micInstance.stop();
-  outputFileStream.end(); // Asegurar que el stream del archivo se cierre
-  console.log("Finalizando grabación, procesando audio...");
+  outputFileStream.end();
+  recordingSpinner.succeed("Finalizando grabación, procesando audio...");
   isRecordingFinished = true;
   tryTranscribe();
   rl.close();
@@ -37,67 +55,32 @@ rl.on("line", () => {
 
 function tryTranscribe() {
   if (!isRecordingFinished) {
-    console.log("La grabación aún no ha terminado. Esperando...");
-    setTimeout(tryTranscribe, 1000); // Espera un segundo antes de reintentar
+    console.log(
+      chalk.magenta("La grabación aún no ha terminado. Esperando...")
+    );
+    setTimeout(tryTranscribe, 1000);
     return;
   }
 
   transcribeAudio("output.wav")
     .then(transcription => {
       console.log("\n");
-      console.log(transcription);
+      console.log(chalk.cyan(transcription));
     })
     .catch(err => {
       if (err.code === "ENOENT") {
         console.error(
-          "Error al transcribir: El archivo no existe. Verifique que la grabación haya terminado."
+          chalk.red(
+            "Error al transcribir: El archivo no existe. Verifique que la grabación haya terminado."
+          )
         );
       } else {
-        console.error("Error al transcribir: ", err.message);
-        console.log("Reintentando transcripción...");
-        setTimeout(tryTranscribe, 5000); // Espera 5 segundos antes de reintentar
+        console.error(
+          chalk.red("Error al transcribir: " + emoji.get("x"), err.message)
+        );
+        console.log(chalk.yellow("Reintentando transcripción..."));
+        // setTimeout(tryTranscribe, 5000);
+        setTimeout(tryTranscribe, 1000);
       }
     });
 }
-
-// import mic from "mic";
-// import fs from "fs";
-// import readline from "readline";
-// import { transcribeAudio } from "./transcriber.js";
-// import micConfig from "./micConfig.js";
-
-// var micInstance = mic(micConfig);
-// var outputFileStream = fs.createWriteStream("output.wav");
-// var micInputStream = micInstance.getAudioStream();
-
-// micInputStream.on("data", function (data) {
-//   console.log(`Grabando... [${data.length} bytes recibidos]`);
-// });
-
-// micInputStream.pipe(outputFileStream);
-
-// console.log("Iniciando grabación... Habla y presiona ENTER para finalizar.");
-
-// micInstance.start();
-
-// console.log("Grabando... Presiona ENTER para detener.");
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// rl.on("line", input => {
-//   micInstance.stop();
-//   console.log("Finalizando grabación, procesando audio...");
-
-//   transcribeAudio("output.wav")
-//     .then(transcription => {
-//       console.log("\n");
-//       console.log(transcription);
-//     })
-//     .catch(err => {
-//       console.error("Error al transcribir: ", err.message);
-//     });
-//   rl.close();
-// });
