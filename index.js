0482ecd0f42b4b42f44f8b18413836c438c167d4
German Burgardt <german@getgloby.com>
2023-12-12 01:26:18 -0300

    best logs

--- index.js
+++ index.js
@@ -12,97 +12,10 @@ var outputFileStream = fs.createWriteStream("output.wav");
 var micInputStream = micInstance.getAudioStream();
 var isRecordingFinished = false;
 
+// Inicia un contador de tiempo
+var startTime = Date.now();
+
 console.clear();
-
-// // barra de progreso
-// micInputStream.on("data", function (data) {
-//   let audioBuffer = new Int16Array(
-//     data.buffer,
-//     data.byteOffset,
-//     data.byteLength / Int16Array.BYTES_PER_ELEMENT
-//   );
-
-//   let sum = 0;
-//   for (let i = 0; i < audioBuffer.length; i++) {
-//     sum += Math.abs(audioBuffer[i]);
-//   }
-//   let average = sum / audioBuffer.length;
-
-//   let maxAmplitude = 32768;
-//   let intensity = (average / maxAmplitude) * 200;
-
-//   // Usar un gradiente de color según la intensidad
-//   let color = intensity > 15 ? "red" : intensity > 7 ? "yellow" : "green";
-//   let intensityBarFilled = chalk[color]("█").repeat(intensity);
-//   let intensityBarEmpty = chalk.grey("▁").repeat(23 - intensity);
-
-//   readline.cursorTo(process.stdout, 0);
-//   process.stdout.write(
-//     `Intensidad: [${intensityBarFilled}${intensityBarEmpty}] `
-//   );
-// });
-
-// // parlante
-// micInputStream.on("data", function (data) {
-//   let audioBuffer = new Int16Array(
-//     data.buffer,
-//     data.byteOffset,
-//     data.byteLength / Int16Array.BYTES_PER_ELEMENT
-//   );
-
-//   let sum = 0;
-//   for (let i = 0; i < audioBuffer.length; i++) {
-//     sum += Math.abs(audioBuffer[i]);
-//   }
-//   let average = sum / audioBuffer.length;
-
-//   let maxAmplitude = 32768;
-//   let intensity = (average / maxAmplitude) * 100;
-
-//   // Asignar un emoji según la intensidad del sonido
-//   let emoji;
-//   if (intensity < 10) {
-//     emoji = "🔈"; // Volumen bajo
-//   } else if (intensity < 30) {
-//     emoji = "🔉"; // Volumen medio
-//   } else if (intensity < 50) {
-//     emoji = "🔊"; // Volumen alto
-//   } else {
-//     emoji = "💥"; // Volumen muy alto
-//   }
-
-//   readline.cursorTo(process.stdout, 0);
-//   process.stdout.write(`Intensidad: ${emoji} `);
-// });
-
-// micInputStream.on("data", function (data) {
-//   let audioBuffer = new Int16Array(
-//     data.buffer,
-//     data.byteOffset,
-//     data.byteLength / Int16Array.BYTES_PER_ELEMENT
-//   );
-
-//   let sum = 0;
-//   for (let i = 0; i < audioBuffer.length; i++) {
-//     // sum += Math.abs(audioBuffer[i]);
-//     sum += Math.abs(audioBuffer[i]) * 10;
-//   }
-//   let average = sum / audioBuffer.length;
-
-//   let maxAmplitude = 32768;
-//   let intensity = (average / maxAmplitude) * 300;
-//   intensity = intensity - 6;
-
-//   // Patrón que se expande y contrae según la intensidad
-//   let pattern = "";
-//   for (let i = 0; i < intensity; i++) {
-//     pattern += "• ";
-//   }
-
-//   readline.cursorTo(process.stdout, 0);
-//   readline.clearLine(process.stdout, 0); // Limpia la línea antes de escribir la nueva salida
-//   process.stdout.write(chalk.blue(`${pattern}`) + "\n");
-// });
 
 micInputStream.on("data", function (data) {
   let audioBuffer = new Int16Array(
@@ -113,23 +26,29 @@ micInputStream.on("data", function (data) {
 
   let sum = 0;
   for (let i = 0; i < audioBuffer.length; i++) {
-    sum += Math.abs(audioBuffer[i]) * 10;
+    sum += Math.abs(audioBuffer[i]);
   }
   let average = sum / audioBuffer.length;
 
   let maxAmplitude = 32768;
-  let intensity = (average / maxAmplitude) * 75; // Ajustar la intensidad a la mitad de la consola
-  intensity = Math.min(intensity, 75); // Asegurarse de que no sobrepase la mitad de la consola
+  let intensity = (average / maxAmplitude) * 300; // Ajustar la intensidad para que sea menor y comience desde el extremo
+  intensity = Math.min(intensity, 50); // Asegurarse de que no sobrepase el nuevo límite establecido
 
-  // Patrón que se expande y contrae según la intensidad, centrado
-  let leftPattern = "• ".repeat(intensity).padStart(75, " ");
-  let rightPattern = "• ".repeat(intensity).padEnd(75, " ");
+  // Patrón que se expande hacia un lado según la intensidad
+  let pattern = "• ".repeat(intensity).padStart(0, " ");
 
   readline.cursorTo(process.stdout, 0);
   readline.clearLine(process.stdout, 0); // Limpia la línea antes de escribir la nueva salida
-  // Combina los patrones con colores diferentes para el efecto de espejo
+
+  // Calcula los segundos transcurridos desde que se inició la grabación
+  const spaceBetwen = 80 - pattern.length;
+  let secondsElapsed = ((Date.now() - startTime) / 1000)
+    .toFixed(2)
+    .padStart(spaceBetwen, " ");
+
+  // Escribe solo el patrón de puntos rojos y los segundos transcurridos al final
   process.stdout.write(
-    chalk.blue(leftPattern) + chalk.red(rightPattern) + "\n"
+    chalk.red(pattern) + " " + chalk.white(secondsElapsed + "s\n")
   );
 });
 
@@ -189,3 +108,187 @@ function tryTranscribe() {
       }
     });
 }
+
+// var micInstance = mic(micConfig);
+// var outputFileStream = fs.createWriteStream("output.wav");
+// var micInputStream = micInstance.getAudioStream();
+// var isRecordingFinished = false;
+
+// console.clear();
+
+// micInputStream.on("data", function (data) {
+//   let audioBuffer = new Int16Array(
+//     data.buffer,
+//     data.byteOffset,
+//     data.byteLength / Int16Array.BYTES_PER_ELEMENT
+//   );
+
+//   let sum = 0;
+//   for (let i = 0; i < audioBuffer.length; i++) {
+//     sum += Math.abs(audioBuffer[i]);
+//   }
+//   let average = sum / audioBuffer.length;
+
+//   let maxAmplitude = 32768;
+//   let intensity = (average / maxAmplitude) * 250; // Ajustar la intensidad para que sea menor y comience desde el extremo
+//   intensity = Math.min(intensity, 50); // Asegurarse de que no sobrepase el nuevo límite establecido
+
+//   // Patrón que se expande hacia un lado según la intensidad
+//   let pattern = "• ".repeat(intensity).padStart(0, " ");
+
+//   readline.cursorTo(process.stdout, 0);
+//   readline.clearLine(process.stdout, 0); // Limpia la línea antes de escribir la nueva salida
+//   // Escribe solo el patrón de puntos rojos
+//   process.stdout.write(chalk.red(pattern) + "\n");
+// });
+
+// micInputStream.pipe(outputFileStream);
+
+// console.log(chalk.green("Recording... " + emoji.get("arrow_forward")));
+
+// console.log("\n");
+// micInstance.start();
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on("line", () => {
+//   micInstance.stop();
+//   outputFileStream.end();
+//   console.log("\n");
+//   const spinner = ora("Processing...").start();
+//   spinner.color = "yellow";
+//   isRecordingFinished = true;
+//   tryTranscribe();
+//   rl.close();
+// });
+
+// function tryTranscribe() {
+//   if (!isRecordingFinished) {
+//     console.log(
+//       chalk.magenta("La grabación aún no ha terminado. Esperando...")
+//     );
+//     setTimeout(tryTranscribe, 1000);
+//     return;
+//   }
+
+//   transcribeAudio("output.wav")
+//     .then(transcription => {
+//       console.log("\n");
+//       console.log(chalk.cyan(transcription));
+//       console.log("\n");
+//       console.log("\n");
+
+//       console.log(chalk.green("Transcription copied to clipboard"));
+//     })
+//     .catch(err => {
+//       if (err.code === "ENOENT") {
+//         console.error(
+//           chalk.red(
+//             "Error al transcribir: El archivo no existe. Verifique que la grabación haya terminado."
+//           )
+//         );
+//       } else {
+//         console.error(
+//           chalk.red("Error al transcribir: " + emoji.get("x"), err.message)
+//         );
+//         console.log(chalk.yellow("Reintentando transcripción..."));
+//         setTimeout(tryTranscribe, 1000);
+//       }
+//     });
+// }
+
+// var micInstance = mic(micConfig);
+// var outputFileStream = fs.createWriteStream("output.wav");
+// var micInputStream = micInstance.getAudioStream();
+// var isRecordingFinished = false;
+
+// console.clear();
+
+// micInputStream.on("data", function (data) {
+//   let audioBuffer = new Int16Array(
+//     data.buffer,
+//     data.byteOffset,
+//     data.byteLength / Int16Array.BYTES_PER_ELEMENT
+//   );
+
+//   let sum = 0;
+//   for (let i = 0; i < audioBuffer.length; i++) {
+//     sum += Math.abs(audioBuffer[i]) * 10;
+//   }
+//   let average = sum / audioBuffer.length;
+
+//   let maxAmplitude = 32768;
+//   let intensity = (average / maxAmplitude) * 75; // Ajustar la intensidad a la mitad de la consola
+//   intensity = Math.min(intensity, 75); // Asegurarse de que no sobrepase la mitad de la consola
+
+//   // Patrón que se expande y contrae según la intensidad, centrado
+//   let leftPattern = "• ".repeat(intensity).padStart(75, " ");
+//   let rightPattern = "• ".repeat(intensity).padEnd(75, " ");
+
+//   readline.cursorTo(process.stdout, 0);
+//   readline.clearLine(process.stdout, 0); // Limpia la línea antes de escribir la nueva salida
+//   // Combina los patrones con colores diferentes para el efecto de espejo
+//   process.stdout.write(
+//     chalk.blue(leftPattern) + chalk.red(rightPattern) + "\n"
+//   );
+// });
+
+// micInputStream.pipe(outputFileStream);
+
+// console.log(chalk.green("Recording... " + emoji.get("arrow_forward")));
+
+// console.log("\n");
+// micInstance.start();
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on("line", () => {
+//   micInstance.stop();
+//   outputFileStream.end();
+//   console.log("\n");
+//   const spinner = ora("Processing...").start();
+//   isRecordingFinished = true;
+//   tryTranscribe();
+//   rl.close();
+// });
+
+// function tryTranscribe() {
+//   if (!isRecordingFinished) {
+//     console.log(
+//       chalk.magenta("La grabación aún no ha terminado. Esperando...")
+//     );
+//     setTimeout(tryTranscribe, 1000);
+//     return;
+//   }
+
+//   transcribeAudio("output.wav")
+//     .then(transcription => {
+//       console.log("\n");
+//       console.log(chalk.cyan(transcription));
+//       console.log("\n");
+//       console.log("\n");
+
+//       console.log(chalk.green("Transcription copied to clipboard"));
+//     })
+//     .catch(err => {
+//       if (err.code === "ENOENT") {
+//         console.error(
+//           chalk.red(
+//             "Error al transcribir: El archivo no existe. Verifique que la grabación haya terminado."
+//           )
+//         );
+//       } else {
+//         console.error(
+//           chalk.red("Error al transcribir: " + emoji.get("x"), err.message)
+//         );
+//         console.log(chalk.yellow("Reintentando transcripción..."));
+//         setTimeout(tryTranscribe, 1000);
+//       }
+//     });
+// }
