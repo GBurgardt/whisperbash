b1fcb099425f3f9f676da0401ec200bfba5603d2
German Burgardt <german@getgloby.com>
2023-12-12 21:18:48 -0300

    save transcriptions

--- index.js
+++ index.js
@@ -2,17 +2,30 @@
 import readline from "readline";
 import micConfig from "./micConfig.js";
 import chalk from "chalk";
+import fs from "fs";
+import path from "path";
 
 import AudioRecorder from "./modules/audio-recorder.js";
 import AudioVisualizer from "./modules/audio-visualizer.js";
 import AudioTranscriber from "./modules/audio-transcriber.js";
 
+// Crear una carpeta de sesión
+function createSessionFolder() {
+  const sessionFolder = `recordings/session_${new Date()
+    .toISOString()
+    .replace(/:/g, "")
+    .replace(/\..+/, "")}`;
+  fs.mkdirSync(sessionFolder, { recursive: true });
+  return sessionFolder;
+}
+
 // Configuración inicial
-let outputPath = "output.wav";
+const sessionFolder = createSessionFolder();
+let outputPath = path.join(sessionFolder, "audio.wav");
 
 let audioRecorder = new AudioRecorder(micConfig, outputPath);
 let audioVisualizer = new AudioVisualizer(Date.now());
-let audioTranscriber = new AudioTranscriber(outputPath);
+let audioTranscriber = new AudioTranscriber(outputPath, sessionFolder);
 
 // Iniciar grabación y visualización
 audioRecorder.startRecording();
@@ -38,3 +51,44 @@ rl.on("line", async () => {
     process.exit(1);
   }
 });
+
+// #!/usr/bin/env node
+// import readline from "readline";
+// import micConfig from "./micConfig.js";
+// import chalk from "chalk";
+
+// import AudioRecorder from "./modules/audio-recorder.js";
+// import AudioVisualizer from "./modules/audio-visualizer.js";
+// import AudioTranscriber from "./modules/audio-transcriber.js";
+
+// // Configuración inicial
+// let outputPath = "output.wav";
+
+// let audioRecorder = new AudioRecorder(micConfig, outputPath);
+// let audioVisualizer = new AudioVisualizer(Date.now());
+// let audioTranscriber = new AudioTranscriber(outputPath);
+
+// // Iniciar grabación y visualización
+// audioRecorder.startRecording();
+// console.log(chalk.green("Recording... "));
+
+// audioRecorder.getAudioStream().on("data", function (data) {
+//   audioVisualizer.visualizeAudio(data);
+// });
+
+// // Manejo de la entrada del usuario para detener la grabación
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on("line", async () => {
+//   audioRecorder.stopRecording();
+//   try {
+//     await audioTranscriber.transcribe();
+//     process.exit(0);
+//   } catch (err) {
+//     console.error(chalk.red("Error during transcription: "), err);
+//     process.exit(1);
+//   }
+// });
--- modules/audio-transcriber.js
+++ modules/audio-transcriber.js
@@ -2,11 +2,13 @@ import ora from "ora";
 import chalk from "chalk";
 import { transcribeAudio } from "../transcriber.js";
 import * as emoji from "node-emoji";
+import path from "path";
+import fs from "fs";
 
 class AudioTranscriber {
-  constructor(outputPath, isRecordingFinished) {
+  constructor(outputPath, sessionFolder) {
     this.outputPath = outputPath;
-    this.isRecordingFinished = isRecordingFinished;
+    this.sessionFolder = sessionFolder;
   }
 
   async transcribe() {
@@ -21,6 +23,12 @@ class AudioTranscriber {
       console.log(chalk.cyan(transcription));
       console.log("\n");
       console.log(chalk.green("Transcription copied to clipboard"));
+
+      const transcriptionPath = path.join(
+        this.sessionFolder,
+        "transcription.txt"
+      );
+      fs.writeFileSync(transcriptionPath, transcription, "utf8");
     } catch (err) {
       spinner.stop();
       console.error(chalk.red("Error during transcription: "), err);
@@ -32,11 +40,7 @@ class AudioTranscriber {
       transcribeAudio(this.outputPath)
         .then(resolve)
         .catch(err => {
-          if (
-            err.code === "ENOENT" &&
-            !this.isRecordingFinished &&
-            retryCount < 3
-          ) {
+          if (err.code === "ENOENT" && retryCount < 3) {
             console.log(chalk.yellow("Reintentando transcripción..."));
             setTimeout(() => {
               this.tryTranscribe(retryCount + 1)
